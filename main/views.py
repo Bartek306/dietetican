24cab9c2b9a9e8d43987d5548d12cf7402dc256b
1mbir <mikoimb99@gmail.com>
2021-06-15 10:21:32 +0200

    merged pdf creator

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
+import mimetypes
+from wsgiref.util import FileWrapper
+
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 # Create your views here.
@@ -7,9 +11,87 @@ from django.contrib.auth import get_user_model
 from .models import Diet, Ingredient, Meal
 from django.contrib.auth.models import Group
 from .decorators import unauthenticated_user, allowed_users
+from fpdf import FPDF
 # user: test passw:testtest12
 # deitetyk eloelo
+
+def meal_list_creator(meals):
+    i = []
+    i_units = []
+    i_values = []
+
+    for meal in meals:
+        for ingr in meal.ingredients.all():
+            i.append(ingr.name)
+            i_units.append(ingr.unit)
+            i_values.append(ingr.amount)
+
+    dict_v = {}
+    dict_u = {}
+
+    for n in i:
+        dict_v[n] = 0
+
+    for n, v, u in zip(i, i_values, i_units):
+        dict_u[n] = u
+        dict_v[n] += v
+
+    return dict_v, dict_u
+
+
+class PDF(FPDF):
+    def titles(self, txt):
+        self.set_xy(0.0, 0.0)
+        self.set_font('sysfont', '', 16)
+        self.set_text_color(220, 50, 50)
+        self.cell(w=210.0, h=40.0, align='C', txt=txt, border=0)
+
+    def texts(self, txt, offset):
+        self.set_xy(10.0, 30.0 + offset * 10)
+        self.set_text_color(76.0, 32.0, 250.0)
+        self.set_font('sysfont', '', 12)
+        self.cell(w=30.0, txt=txt)
+
+    def names(self, txt, offset):
+        self.set_xy(5.0, 10.0 + offset * 10)
+        self.set_font('sysfont', '', 16)
+        self.set_text_color(220, 50, 50)
+        self.cell(w=30.0, h=40.0, align='C', txt=txt, border=0)
+
+    def create_shopping_list(self, meals, filename):
+        self.add_font('sysfont', '', r"c:\WINDOWS\Fonts\arial.ttf", uni=True)
+        self.add_page()
+        self.titles("Lista zakupów")
+        dict_v, dict_u = meal_list_creator(meals)
+        arr = dict_v.keys()
+        value = 2
+        for name in arr:
+            self.texts(" - " + name + " " + str(dict_v[name]) + " " + str(dict_u[name]), value)
+            value += 1
+
+        self.output(filename)
+
+    def create_pdf(self, meals, filename):
+        self.add_font('sysfont', '', r"c:\WINDOWS\Fonts\arial.ttf", uni=True)
+        self.add_page()
+        self.titles("Dieta")
+
+        for meal in meals:
+            self.add_page()
+            self.titles(str(meal.name))
+            self.names("Skladniki: ", 1)
+            count = 0
+            for ingr in meal.ingredients.all():
+                self.texts(str(ingr), 2 + count)
+                count += 1
+
+            self.names("Przepis: ", 2 + len(meal.ingredients.all()))
+            self.texts(str(meal.recipe), 3 + len(meal.ingredients.all()))
+
+        self.output(filename, 'F')
+
 User = get_user_model()
+
 
 
 @allowed_users(allowed_roles=['Clients'])
@@ -32,6 +114,10 @@ def home(request):
 			ingradients.append(lst)
 			lst = ""
 		my_list = zip(meals, ingradients)
+		pdf1 = PDF()
+		pdf1.create_pdf(meals, "diet.pdf")
+		pdf2 = PDF()
+		pdf2.create_shopping_list(meals, "shopping_list.pdf")
 	else:
 		my_list = None
 	return render(request, "home.html", {"my_list": my_list})
@@ -114,6 +200,28 @@ def client(request, username):
 	return render(request, "client.html", {"my_list": my_list,
 											"username": t[0],
 											"names": name})
+
+def download_diet(request):
+    fl_path = 'diet.pdf'
+
+    fl = open(fl_path, 'rb')
+    wrapper = FileWrapper(fl)
+    mime_type, _ = mimetypes.guess_type(fl_path)
+    response = HttpResponse(wrapper, content_type=mime_type)
+    response['Content-Disposition'] = 'attachment; filename=%s' % 'dieta.pdf'
+    return response
+
+
+def download_shopping_list(request):
+    fl_path = 'shopping_list.pdf'
+
+    fl = open(fl_path, 'rb')
+    wrapper = FileWrapper(fl)
+    mime_type, _ = mimetypes.guess_type(fl_path)
+    response = HttpResponse(wrapper, content_type=mime_type)
+    response['Content-Disposition'] = 'attachment; filename=%s' % 'lista_zakupow.pdf'
+    return response
+
 @csrf_exempt
 @unauthenticated_user
 @allowed_users(allowed_roles=['Dietitians'])
